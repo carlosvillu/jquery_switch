d095ca1bcd782b6ba570ee94b2364885ab121708
Carlos Villuendas <carlosvillu@gmail.com>
2010-10-10 19:46:00 +0200

    Add firsts test at the plugin

--- src/jquery.switch.js
+++ src/jquery.switch.js
@@ -7,3 +7,11 @@ Licensed under the MIT license.
 
 */
 
+jQuery.widget('carlosvillu.jswitch', {
+	_init: function(){
+
+		this._switch = jQuery('<div/>').attr('class', 'switch');
+		jQuery(this.element).append(this._switch);
+	}
+});
+
---
+++ test/tests.js
@@ -0,0 +1,26 @@
+// Contenedor del swotch para todas las pruebas
+var switch_container = jQuery('<div/>').attr('id', 'switch_container');
+
+module('jQuery Swtich', {
+	setup: function(){
+		jQuery('#qunit-fixture').append(switch_container);
+	},
+	teardown: function(){
+		jQuery(switch_container).remove();
+	}
+});
+test('Existe un función en el espacio de nombre de jQuery', 1, function(){
+	ok( typeof(jQuery.fn.jswitch) === 'function', 'No existe una función para utilizar el plugin');
+});
+
+test('Agregamos un Switch en un capa', 2, function(){
+	jQuery(switch_container).jswitch();
+	ok(!!jQuery('.switch'), 'No hay ningún elemento switch');
+	equals(jQuery('.switch').parent()[0], jQuery(switch_container)[0], 'El switch no esta dentro de la capa contenedora')
+});
+test('Intentamos de agregar el swicth a un contenedor que no es un DIV', function(){
+	var p = jQuery('<p/>');
+	jQuery(switch_container).append(p);
+	jQuery(p).jswitch();
+	equals(jQuery('.switch').length, 0, 'Se ha agregado un switch a un elemento <p>');
+});
