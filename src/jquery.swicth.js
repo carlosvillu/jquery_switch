840c2882c410cf69c87329a7369e84c2d4abf89d
Carlos Villuendas <carlosvillu@gmail.com>
2010-10-10 13:07:15 +0200

    Modificed license and ignorated textmate files

---
+++ src/jquery.swicth.js
@@ -0,0 +1,9 @@
+/** 
+jQuery Switch Plugin
+http://github.com/carlosvillu/jquery_switch
+
+Copyright (c) 2010 Carlos Villuendas Zambrana (http://www.carlosvillu.com)
+Licensed under the MIT license.
+
+*/
+
