a7261121d1dfd47862b25ed4d29c1acfca9d6a3e
Carlos Villuendas <carlosvillu@gmail.com>
2010-10-10 21:31:52 +0200

    Add check for only create swicht in div elements

--- src/jquery.switch.js
+++ src/jquery.switch.js
@@ -9,7 +9,10 @@ Licensed under the MIT license.
 
 jQuery.widget('carlosvillu.jswitch', {
 	_init: function(){
-
+		if(this.element[0] == jQuery('p')[0]){
+			jQuery(this.element).html('mandatory swicth container is a div');
+			return false;
+		}
 		this._switch = jQuery('<div/>').attr('class', 'switch');
 		jQuery(this.element).append(this._switch);
 	}
--- test/tests.js
+++ test/tests.js
@@ -14,13 +14,16 @@ test('Existe un función en el espacio de nombre de jQuery', 1, function(){
 });
 
 test('Agregamos un Switch en un capa', 2, function(){
+	
 	jQuery(switch_container).jswitch();
 	ok(!!jQuery('.switch'), 'No hay ningún elemento switch');
-	equals(jQuery('.switch').parent()[0], jQuery(switch_container)[0], 'El switch no esta dentro de la capa contenedora')
+	equals(jQuery('.switch').parent()[0], jQuery(switch_container)[0], 'El switch no esta dentro de la capa contenedora');
+	
 });
-test('Intentamos de agregar el swicth a un contenedor que no es un DIV', function(){
+test('Intentamos agregar el swicth a un contenedor que no es un DIV', function(){
 	var p = jQuery('<p/>');
 	jQuery(switch_container).append(p);
 	jQuery(p).jswitch();
-	equals(jQuery('.switch').length, 0, 'Se ha agregado un switch a un elemento <p>');
+	equals(jQuery('.switch').length, 1, 'Se ha agregado un switch a un elemento <p>');
+	jQuery(switch_container).remove();
 });
